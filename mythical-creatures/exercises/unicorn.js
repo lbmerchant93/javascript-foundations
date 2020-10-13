5b6286bd6a943a63456a51d4ef07185e76eb6d35
Lucas Merchant <lbmerchant93@gmail.com>
2020-10-13 13:38:52 -0600

    Initial commit. Add functions and code to unicorn.js to pass unicorn-test.js

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,21 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
     this.color = color;
+    }
+  }
+  isWhite (color) {
+    if (color !== 'white') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  says (saying) {
+    return `**;* ${saying} *;**`
   }
 }
 
