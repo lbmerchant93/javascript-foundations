68ae92e50bc0fc5687750ce9ce6f6bc3b654994c
Lucas Merchant <lbmerchant93@gmail.com>
2020-10-14 20:43:01 -0600

    Add code to Dragon class to pass dragon-test.js

---
+++ mythical-creatures/exercises/dragon.js
@@ -0,0 +1,17 @@
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.hungry = true;
+    this.count = 0;
+  };
+
+  eat () {
+    this.count+= 1;
+    if (this.count === 3){
+     return this.hungry = false;
+   }
+  }
+}
+module.exports = Dragon;
